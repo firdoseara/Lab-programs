ee14a177a95dae92950dfbf11934025299c9d0a8
firdoseara <143375967+firdoseara@users.noreply.github.com>
2024-06-21 15:28:15 +0530

    7a.jl

---
+++ Julia lab/7a.jl
@@ -0,0 +1,28 @@
+using Printf
+function tolowercase(ch::Char)
+        if isletter(ch)
+                return lowercase(ch)
+        end
+        return '0'
+end
+function LetterFrequency(LetterFreq::Dict,inn)
+        while !eof(inn)
+                ch=read(inn,Char)
+                if(c=tolowercase(ch))!='0'
+                        LetterFreq[c]=get(LetterFreq,c,0)+1
+                end
+        end
+end
+function main()
+        inn=open("input7.txt","r")
+        out=open("output7.txt","w")
+        LetterFreq=Dict{Char,Int64}()
+        LetterFrequency(LetterFreq,inn)
+        println(out,"Letter Frequency\n")
+        for ch in sort(collect(keys(LetterFreq)))
+                @printf(out,"%4c%8d\n",ch,LetterFreq[ch])
+        end
+        close(inn)
+        close(out)
+end
+main()
